d81042a28e4c6144cf04848a18f8064a62984482
= <=>
2026-01-11 12:01:03 -0600

    Add unit clause resolution

--- src/lib.rs
+++ src/lib.rs
@@ -4,45 +4,89 @@ mod tests;
 use pyo3::prelude::*;
 
 
-// Representation of a boolean expression in conjunctive normal form
-
-/// A Python module implemented in Rust.
 #[pymodule]
 pub mod easypls {
     use pyo3::prelude::*;
     use std::sync::Arc;
 
+    // Representation of a boolean expression in conjunctive normal form
     #[pyclass]
-    struct CNF {
-        // symbol_table[id] represents symbol of variable with id
+    pub struct CNF {
+        // Symbol_table[id - 1] represents symbol of variable with id
+        // We define it this way because 0 isn't distict from -0
         symbol_table: Arc<Vec<String>>,
 
         // Outer list represents conjunction of inner lists which
         // represent disjunctions of variables, represented by their ids
-        cnf: Vec<Vec<usize>>,
+        // -id represents negation
+        clauses: Vec<Vec<isize>>,
     }
 
-    #[pymethods]
     impl CNF {
-        fn find_unit_clauses(&self) -> Vec<usize> {
+        // Return list of all unit clauses
+        pub fn find_unit_clauses(&self) -> Vec<isize> {
             let mut unit_clauses = Vec::new();
-            for clause in self.cnf.iter() {
-                if clause.len() == 0 {
+            for clause in self.clauses.iter() {
+                if clause.len() == 1 {
                     unit_clauses.push(clause[0]);
                 }
             }
             unit_clauses
         }
 
-        // Returns whether a CNF is satisfiable
-        // if so we return the signature that makes the CNF true.
-        // otherwise we return None
-        fn is_sat_aux(&self, conditioned: usize) -> Option<Vec<usize>> {
+        pub fn new(symbol_table: Arc<Vec<String>>, clauses: Vec<Vec<isize>>) -> CNF {
+            CNF {symbol_table, clauses }
+        }
+
+        pub fn get_clauses_clone(&self) -> Vec<Vec<isize>> {
+            self.clauses.clone()
+        }
+
+        // Create new CNF with same symbol table
+        pub fn from_self(&self, clauses: Vec<Vec<isize>>) -> CNF {
+            CNF { symbol_table: Arc::clone(&self.symbol_table), clauses }
+        }
+
+        // Return a CNF after conditioning on some variable target
+        // TODO optimize with binary search
+        pub fn conditioned(&self, target: isize) -> CNF {
+            let mut new_clauses = Vec::new();
+
+            'clause_loop: for clause in self.clauses.iter() {
+                let mut new_clause = Vec::new();
+
+                for var in clause {
+                    if *var == target {
+                        break 'clause_loop;
+                    }
+                    if *var != -target {
+                        new_clause.push(*var)
+                    }
+                }
+
+                new_clauses.push(new_clause);
+            }
+
+            self.from_self(new_clauses)
+        }
+        
+        // Returns if CNF is sat, takes the variable we want to condition on
+        fn is_sat_helper(&self, current: isize) -> Option<Vec<usize>> {
             // Unit propigation
             // Check if there are no clauses remaining, if so return true
             // Check if there are any empty clauses, if so return false
             // Condition on the next variable
             todo!()
+        }
+    }
+
+    #[pymethods]
+    impl CNF {
+        // Returns whether a CNF is satisfiable
+        // if so we return the signature that makes the CNF true.
+        // otherwise we return None
+        pub fn is_sat(&self) -> Option<Vec<usize>> {
+            self.is_sat_helper(1)
         }
     }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,6 +1,22 @@
 use crate::easypls::*;
 
+use std::sync::Arc;
+
 #[test]
-fn some_test() {
-    assert!(sum_as_string(1, 2).unwrap() == "3");
+fn unit_clause_finding_and_resolution() {
+    let symbol_table = vec![String::from("x"), String::from("y"), String::from("z")];
+    let symbol_table = Arc::new(symbol_table);
+
+    let cnf = CNF::new(Arc::clone(&symbol_table), vec![vec![1], vec![1, -2]]);
+    let unit_clauses = cnf.find_unit_clauses();
+
+    assert_eq!(unit_clauses, vec![1]);
+    assert_eq!(cnf.conditioned(1).get_clauses_clone(), Vec::<Vec<isize>>::new());
+
+
+    let cnf = CNF::new(Arc::clone(&symbol_table), vec![vec![1, 2, -3], vec![-2], vec![3]]);
+    let unit_clauses = cnf.find_unit_clauses();
+
+    assert_eq!(unit_clauses, vec![-2, 3]);
+    assert_eq!(cnf.conditioned(-2).conditioned(3).get_clauses_clone(), vec![vec![1]]);
 }
